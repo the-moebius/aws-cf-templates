f81955163f34cd5341b6a5c2c107f0aa25cc6e09
Slava Fomin II <slava@fomin.io>
2019-10-27 15:22:29 +0300

    Added utilities for templates deployment

---
+++ bin/deploy-templates.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+ROOT_PATH="$(dirname "$0")/.."
+PROJECT_NAME="${1:-moebius}"
+STACK_NAME="${PROJECT_NAME}-aws-cf-templates"
+
+echo "Deploying reusable AWS CloudFront templates…"
+
+aws cloudformation deploy \
+  --template-file "${ROOT_PATH}/provision/resources.template" \
+  --stack-name "${STACK_NAME}"
+
+echo "Syncing the templates…"
+
+aws s3 sync "${ROOT_PATH}/templates" "s3://${STACK_NAME}"
